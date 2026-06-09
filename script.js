e4d19b2d6f540831cdddda019d8d0669d22d7341
jrobbins2006 <jrobbins2006@gmail.com>
2026-06-09 10:07:39 -0400

    fixed errors

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const emailFeild = document.getElementById('email')
+const emailField = document.getElementById('email')
 const errorIcon = document.getElementById('error-icon')
 const errorMessage = document.getElementById('error-message')
 function submitEmail (event) {
   event.preventDefault()
-  let emailAddress = emailFeild.value
-  let validEmailAddress = emailFeild.validity.valid
+  let emailAddress = emailField.value
+  let validEmailAddress = emailField.validity.valid
   if (!validEmailAddress) {
     errorIcon.style.display = 'block'
     errorMessage.style.display = 'block'
   } else {
     errorIcon.style.display = 'none'
     errorMessage.style.display = 'none'
-    console.log(validEmailAddress)
-    console.log('done')
   }
 }
